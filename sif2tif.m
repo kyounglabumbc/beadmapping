632559046d35e5d298aa4325c393ac06d0c814e7
Matt <mattvlaw@gmail.com>
2015-05-27 11:52:10 -0400

    fixed a bug where sif2tif was not updated to prompt independently for filename from readsif

--- sif2tif.m
+++ sif2tif.m
@@ -1,7 +1,11 @@
 
-[A, fname] = readSif();
+
+ [FileName,PathName,FilterIndex]=uigetfile('*.sif;*.SIF', 'Select SIF'); % opens dialog to let the user select sif to open
+fname=strcat(PathName,FileName);
+ A = readSif(fname);    
 A = rot90(A);
 avgA = mean(A,3);
 X16 = uint16(3*avgA-1);
 avgA = im2uint8(X16);
-imwrite(avgA,strcat('C:\User\tir data\' ,fname(1),'_ave.tif'), 'tif');
+[pathstr,fnameonly,ext] = fileparts(fname); 
+imwrite(avgA,strcat('C:\User\tir data\' ,fnameonly,'_ave.tif'), 'tif');
